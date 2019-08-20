05f813d455686d911db2bbd33518cd27a3c98942
Mariano <marianomiranda0019@gmail.com>
2019-08-20 16:13:27 -0700

    This is the rest of the stuff

---
+++ main.py
@@ -0,0 +1,34 @@
+import webapp2
+import jinja2
+import os
+
+the_jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MainPageHandler(webapp2.requestHandler):
+    def get(self):
+        result_template = the_jinja_env.get_template('template/index.html')
+        self.response.write(result_template.render())
+
+class LogInPageHandler(webapp2.requesthandler):
+    def get(self):
+        result_template = the_jinja_env.get_template('template/logInPage.html')
+        self.response.write(result_template.render())
+
+class SignUpPageHandler(webapp2.requesthandler):
+    def get(self):
+        result_template = the_jinja_env.get_template('template/logInPage.html')
+        self.response.write(result_template.render())
+        def post(self):
+            form
+
+
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPageHandler),
+    ('/logInPage', LogInPageHandler),
+    ('/signUpPage', SignUpPageHandler)
+], debug = True)
