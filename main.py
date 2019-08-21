f2e8b64d82bc8c5accbfafc5f9af7ebbf7d60e01
alozada4 <alozada4@toromail.csudh.edu>
2019-08-21 10:35:32 -0700

    fixed main.py

--- main.py
+++ main.py
@@ -7,25 +7,26 @@ the_jinja_env = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-class MainPageHandler(webapp2.requestHandler):
+class MainPageHandler(webapp2.RequestHandler):
     def get(self):
         result_template = the_jinja_env.get_template('templates/index.html')
         self.response.write(result_template.render())
 
-class LogInPageHandler(webapp2.requesthandler):
+class LogInPageHandler(webapp2.RequestHandler):
     def get(self):
         result_template = the_jinja_env.get_template('templates/logInPage.html')
         self.response.write(result_template.render())
 
-class SignUpPageHandler(webapp2.requesthandler):
-    def get(self):
-        result_template = the_jinja_env.get_template('templates/logInPage.html')
-        self.response.write(result_template.render())
-        def post(self):
-            form
-
-
-
+class SignUpPageHandler(webapp2.RequestHandler):
+    def post(self):
+        result_template = the_jinja_env.get_template('templates/SignUpPageHandler.html')
+        user_name = self.request.get('username')
+        Pass_Word = self.request.get('password')
+        new_dict = {
+        "username": user_name,
+        "password": pass_Word
+    }
+        self.response.write(result_template.render(new_dict))
 
 app = webapp2.WSGIApplication([
     ('/', MainPageHandler),
